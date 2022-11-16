797b2b5137cff01f2ccd193eb855a8162cab7c86
Moina Vernon <mvernon@cpp.edu>
2022-11-15 21:50:13 -0800

    add create note fragment

--- app/src/main/java/com/bignerdranch/android/notesapp/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/notesapp/MainActivity.kt
@@ -11,15 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        replaceFragment(HomeFragment.newInstance(),false)
+        replaceFragment(HomeFragment.newInstance(),false)
     }
 
-//    fun replaceFragment(fragment:Fragment, istransition:Boolean){
-//        val fragmentTransition = supportFragmentManager.beginTransaction()
-//
-//        if (istransition){
-//            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
-//        }
-//        fragmentTransition.add(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
-//    }
+    fun replaceFragment(fragment:Fragment, istransition:Boolean){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+
+        if (istransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.add(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
 }
